064283651b0336c4ab035f12c39f5c0e258b1ba7
Cameron Henlin <cam.henlin@gmail.com>
2015-02-12 19:31:22 -0800

    still not working just switching computers

--- app.js
+++ app.js
@@ -12,19 +12,26 @@ array('addObject', $('+15415809414'));
 console.log(array);
 
 // set up the app delegate
-var CompletionObj = $.NSObject('alloc')('init');
-CompletionObj.addMethod('completionBlock:', 'v@:@', function (self, _cmd, notif) {
-	console.log(self);
-	console.log(_cmd);
-	console.log(notif);
+var CompletionObj = $.NSObject.extend('MessagesKit');
+CompletionObj.addMethod('completionBlock:', 'v@:@', function (self, _cmd, notif, etc) {
+
 	// var systemStatusBar = $.NSStatusBar('systemStatusBar');
 	// statusMenu = systemStatusBar('statusItemWithLength', $.NSVariableStatusItemLength);
 	// statusMenu('retain');
 	// var title = $.NSString('stringWithUTF8String', "Hello World");
 	// statusMenu('setTitle', title);
 });
+CompletionObj.register();
+// - (void)startNewConverstaionInMessages; // just lol at the typo in Apple's headers
+messageKit("startNewConverstaionInMessages");
+
+console.log('start new conversation successful i guess?');
+
+console.log(messageKit("iMessageURLForAddress", $('cam.henlin@gmail.com')));
+
+// + (id)iMessageURLForAddress:(id)arg1;
 
 // - (void)sendMessageText:(NSString *)arg1 toRecipients:(NSArray *)arg2 onService:(NSString *)arg3;
-messageKit("sendMessageText", "test", "toRecipients", array, "withCompletionBlock", CompletionObj('completionBlock'));
+messageKit("sendMessageText", $("test"), "toRecipients", $("+15415809414"), "onService", $("iMessage"));
 
 console.log('loaded framework');
